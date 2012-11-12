b2087ec9bb04acdb748eff98f723152cf31bbfbb
Airton <airton@a2comunicacao.com.br>
2012-11-12 18:56:20 -0200

    Montagem demos

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="pt-BR">
 <head>
-	<meta charset="UTF-8">
+	<meta charset="ISO-8859-1">
 	<title>CD-ROM Framework</title>
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/estilo.css">
@@ -13,15 +13,22 @@
 	<div class="container">
 		<p>
 			<a href="cd/fotos/" class="btn btn-large">CD/DVD de Fotos</a>
+			<a href="cd/fotos/" class="btn btn-large btn-laranja">Demo</a>
 		</p>
 		<p>
-			<a href="cd/albuns/" class="btn btn-large">CD/DVD de √Ålbuns</a>
+			<a href="cd/albuns/" class="btn btn-large">CD/DVD de ¡lbuns</a>
+			<a href="cd/albuns/" class="btn btn-large btn-laranja">Demo</a>
 		</p>
 		<p>
-			<a href="cd/videos/" class="btn btn-large">CD/DVD de V√≠deos</a>
+			<a href="cd/videos/" class="btn btn-large">CD/DVD de VÌdeos</a>
+			<a href="cd/videos/" class="btn btn-large btn-laranja">Demo</a>
 		</p>
 		<p>
 			<a href="cd/especial/" class="btn btn-large">CD/DVD Especial</a>
+			<a href="cd/especial/" class="btn btn-large btn-laranja">Demo</a>
+		</p>
+		<p>
+			<a href="cd/capas-bolachas/" class="btn btn-large">CD/DVD Capas/Bolachas</a>
 		</p>
 	</div>
 	<footer>
