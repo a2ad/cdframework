a4c58b9ae495a3abe71943a62758e542dea00c16
Airton <airton@a2comunicacao.com.br>
2012-11-13 18:15:15 -0200

    Montagem cd especiais

---
+++ cd/especial/fotos.js
@@ -0,0 +1,22 @@
+jQuery(function($){
+
+	$.supersized({
+		autoplay: 0,
+		slides:  	[			// Imagens do slideshow
+
+		{
+			image : 'conteudo/fotos/nome-da-imagem.jpg',
+			url: 	'conteudo/fotos/nome-da-imagem.jpg',
+			title : 'Inserir o Título da Imagem'
+		},
+		{
+			image : 'conteudo/fotos/nome-da-imagem.jpg',
+			url: 	'conteudo/fotos/nome-da-imagem.jpg',
+			title : 'Inserir o Título da Imagem'
+		}
+
+		]
+
+		
+	});
+});
