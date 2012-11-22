7fb7b002346afc037c6433d5f8020bfd4be42f62
Airton <airton@a2comunicacao.com.br>
2012-11-22 15:14:01 -0200

    Ajuste título site

--- index.php
+++ index.php
@@ -34,5 +34,56 @@
 	<footer>
 		
 	</footer>
+
+ 
+	<?php
+
+	$diretorio = 'cd/'; 
+	$ponteiro  = opendir($diretorio);
+	while ($nome_itens = readdir($ponteiro)) {
+	    $itens[] = $nome_itens;
+	}
+
+	sort($itens);
+	foreach ($itens as $listar) {
+	   if ($listar!="." && $listar!=".."){ 
+	   		if (is_dir($listar)) { 
+				$pastas[]=$listar; 
+			} else{ 
+				$arquivos[]=$listar;
+			}
+	   }
+	}
+
+	if ($arquivos != "") {
+		foreach($arquivos as $listar){
+
+		   	// pega o endereço do diretório
+			$diretorio = 'cd/'.$listar; 
+			echo $diretorio.'<br>';
+
+			// inicializa a classe ZipArchive
+			$zip = new ZipArchive();
+			// abre o arquivo .zip
+			if ($zip->open($listar.".rar", ZIPARCHIVE::CREATE) !== TRUE) {
+			die ("Erro!");
+			}
+
+			$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($diretorio));
+			echo $iterator;
+
+			// itera cada pasta/arquivo contido no diretório especificado
+			foreach ($iterator as $key=>$value) {
+			// adiciona o arquivo ao .zip
+			$zip->addFile(realpath($key), iconv('ISO-8859-1', 'IBM850', $key)) or die ("ERRO: Não é possível adicionar o arquivo: $key");
+			}
+			// fecha e salva o arquivo .zip gerado
+			$zip->close();
+		}
+
+    }
+
+
+	?>
 </body>
 </html>
