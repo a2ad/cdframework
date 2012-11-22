600a076639014c5ff851535ab2981600efd9534c
Airton <airton@a2comunicacao.com.br>
2012-11-22 12:10:59 -0200

    Montagem cd de vídeos

--- cd/videos/js/scripts.js
+++ cd/videos/js/scripts.js
@@ -1,5 +1,16 @@
 $(function(){
 
-	
+	//GERAL
+	var janela = $(window);
+	var larguraDisponivel = $(janela).innerWidth();
+	var alturaDisponivel = $(janela).innerHeight();
+
+	$('.html5lightbox').each(function(){
+		$(this).attr({
+			'data-width': larguraDisponivel -200,
+			'data-height': alturaDisponivel -200,
+		});
+		
+	});
 
 });
