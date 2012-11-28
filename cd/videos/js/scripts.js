dcbc598ce3d52eb2d5a9b33be057c6d0423c19d0
airton <airtonvancin@gmail.com>
2012-11-28 18:06:09 -0200

    Ajustes para alinhar thumbs no centro

--- cd/videos/js/scripts.js
+++ cd/videos/js/scripts.js
@@ -13,4 +13,24 @@ $(function(){
 		
 	});
 
+	//THUMBS
+	var larguraContainer = parseInt($('.container').width());	
+	var larguraThumb = $('.thumb-video').innerWidth();
+	var totalThumbs = parseInt(larguraContainer / larguraThumb);
+	var larguraTotalThumbs = larguraThumb * totalThumbs;
+	var sobra = larguraContainer - larguraTotalThumbs;
+	var margin = sobra / totalThumbs;
+	var marginLeft = margin / 2 + 'px';
+	var marginRight = margin / 2 + 'px';
+
+	//console.log(larguraContainer +' - '+ larguraThumb +' = '+ totalThumbs +' - '+ larguraTotalThumbs +' - '+ sobra  +' - '+ margin +' - '+ marginLeft);
+
+	$('.thumb-video').each(function(){
+		$(this).css({
+			'margin-left': marginLeft,
+			'margin-right': marginRight,
+			'margin-bottom': '35px'
+		});
+	});
+
 });
